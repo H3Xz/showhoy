e3f8defc0e74a23256a14af94ff7649b49b343ff
GJnaja <kingzjoker@gmail.com>
2023-04-26 19:16:23 +0700

    13th

--- wp-content/plugins/sh-plugin/js/show_result.js
+++
@@ -1,8 +0,0 @@
-// // สร้าง popup ด้วย window.open()
-// var myWindow = window.open("", "myPopup", "width=400,height=300");
-
-// // กำหนดข้อความที่จะแสดงใน popup
-// myWindow.document.write("<p>ผลลัพธ์: " + result + "</p>");
-
-// // ปิด popup เมื่อคลิกที่ปุ่ม "ปิด"
-// myWindow.document.write("<button onclick='window.close()'>ปิด</button>");
